8f07eeccba6caf00a8b000302919983ba41588ef
Sanjay-Ramesh <sanjayramesh2111@gmail.com>
2026-06-16 11:35:03 +0530

    Phase 8: Dashboard with leave balance cards complete

--- worknest_frontend/src/components/Sidebar.jsx
+++ worknest_frontend/src/components/Sidebar.jsx
@@ -21,3 +21,4 @@ function Sidebar() {
 }
 
 export default Sidebar;
+
--- worknest_frontend/src/pages/Dashboard.jsx
+++ worknest_frontend/src/pages/Dashboard.jsx
@@ -1,15 +1,51 @@
 import {jwtDecode} from "jwt-decode";
 import Sidebar from "../components/Sidebar";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Dashboard() {
     const token = localStorage.getItem("token");
     const decoded = jwtDecode(token);
+    const employeeId = decoded.employeeId;
+
+    const [balance, setBalance] = useState(null);
+
+    useEffect(() => {
+        const fetchBalance = async () => {
+             const response = await axios.get("http://localhost:8080/api/leaves/balance", {
+            params:{ employeeId : employeeId},
+            headers:{ Authorization: `Bearer ${token}` }
+        })
+        setBalance(response.data);
+        }
+        fetchBalance()
+    }, []);
 
     return (
         <div className="flex min-h-screen">
             <Sidebar />
-            <div className="bg-gray-50 flex-1">
+            
+            <div className="bg-gray-50 flex-1 p-8">
                 <h1>Welcome, {decoded.name}</h1>
+                {balance && (
+                    <div className="flex gap-4 mt-6">
+                        <div className="bg-white p-6 rounded-lg shadow w-48">
+                            <p className="text-gray-500 text-sm">Casual Leave</p>
+                            <p className="text-3xl font-bold text-blue-600">{balance.casual.remaining}</p>
+                            <p className="text-gray-400 text-xs">of {balance.casual.total} remaining</p>
+                        </div>
+                        <div className="bg-white p-6 rounded-lg shadow w-48">
+                            <p className="text-gray-500 text-sm">Sick Leave</p>
+                            <p className="text-3xl font-bold text-blue-600">{balance.sick.remaining}</p>
+                            <p className="text-gray-400 text-xs">of {balance.sick.total} remaining</p>
+                        </div>
+                        <div className="bg-white p-6 rounded-lg shadow w-48">
+                            <p className="text-gray-500 text-sm">Earned Leave</p>
+                            <p className="text-3xl font-bold text-blue-600">{balance.earned.remaining}</p>
+                            <p className="text-gray-400 text-xs">of {balance.earned.total} remaining</p>
+                        </div>
+                    </div>
+                )}
                 <button>Apply Leave</button>
             </div>
         </div>
